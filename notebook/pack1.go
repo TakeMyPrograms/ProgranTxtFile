ac8b52f1ba88a3304377e5457e91d2c799baa436
ManakovaElena <yalena.mmm@yandex.ru>
2024-10-19 21:41:44 +0400

    create initial pack1

---
+++ notebook/pack1.go
@@ -0,0 +1,14 @@
+package notebook
+import(
+"log/slog"
+)
+func Nop(){
+slog.Info(
+"Execution process",
+slog.String("package", "notebook"),
+slog.String("function", "Nop"),
+slog.Int("status", 200)
+}
+func init(){
+slog.Debug("init() of notebook/notebook.go")
+}
