f7b69146249145d0c78bb09aefbe63086bb37a72
ManakovaElena <yalena.mmm@yandex.ru>
2024-10-19 22:51:10 +0400

    correct pack1.go

--- notebook/pack1.go
+++ notebook/pack1.go
@@ -3,11 +3,11 @@ import(
 "log/slog"
 )
 func Nop(){
-slog.Info(
-"Execution process",
-slog.String("package", "notebook"),
-slog.String("function", "Nop"),
-slog.Int("status", 200)
+	slog.Info(
+	"Execution process",
+	slog.String("package", "notebook"),
+	slog.String("function", "Nop"),
+	slog.Int("status", 200))
 }
 func init(){
 slog.Debug("init() of notebook/notebook.go")
